cf7c77d48392468a4bac3d06c5b5d38e873e1c60
Charles Lapierre <charles.lapierre@infinite-lab.com>
2025-06-07 22:25:07 +0200

    refactor: update stubsRoot path to include 'stubs' directory

--- stubs/main.ts
+++ stubs/main.ts
@@ -1,7 +1,8 @@
 import { getDirname } from '@adonisjs/core/helpers'
+import path from 'node:path'
 
 /**
  * Path to the root directory where the stubs are stored. We use
  * this path within commands and the configure hook
  */
-export const stubsRoot = getDirname(import.meta.url)
+export const stubsRoot = path.join(getDirname(import.meta.url), 'stubs')
