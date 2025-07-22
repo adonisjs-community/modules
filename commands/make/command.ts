687f77397e32b1bf505420b706d0027757d50551
Charles Lapierre <charles.lapierre@infinite-lab.com>
2025-07-22 11:35:38 +0200

    fix: disable make:command

--- commands/make/command.ts
+++ commands/make/command.ts
@@ -1,29 +1,29 @@
-import { stubsRoot } from '../../stubs/main.js'
-import MakeCommand from '@adonisjs/core/commands/make/command'
-import { flags } from '@adonisjs/core/ace'
-import { COMMAND_PREFIX, MODULE_FLAG } from '../../src/constants.js'
-import { checkModule } from '../../src/utils.js'
+// import { stubsRoot } from '../../stubs/main.js'
+// import MakeCommand from '@adonisjs/core/commands/make/command'
+// import { flags } from '@adonisjs/core/ace'
+// import { COMMAND_PREFIX, MODULE_FLAG } from '../../src/constants.js'
+// import { checkModule } from '../../src/utils.js'
 
 /**
  * Make a new ace command
  */
-export default class MMakeCommand extends MakeCommand {
-  @flags.string(MODULE_FLAG)
-  declare module: string
+// export default class MMakeCommand extends MakeCommand {
+//   @flags.string(MODULE_FLAG)
+//   declare module: string
 
-  override async run() {
-    if (!this.module) {
-      return super.run()
-    }
+//   override async run() {
+//     if (!this.module) {
+//       return super.run()
+//     }
 
-    if (!checkModule(this.app, this.module)) {
-      this.kernel.exec(`${COMMAND_PREFIX}:module`, [this.module])
-    }
+//     if (!checkModule(this.app, this.module)) {
+//       this.kernel.exec(`${COMMAND_PREFIX}:module`, [this.module])
+//     }
 
-    const codemods = await this.createCodemods()
-    await codemods.makeUsingStub(stubsRoot, this.stubPath, {
-      flags: this.parsed.flags,
-      entity: this.app.generators.createEntity(this.name),
-    })
-  }
-}
+//     const codemods = await this.createCodemods()
+//     await codemods.makeUsingStub(stubsRoot, this.stubPath, {
+//       flags: this.parsed.flags,
+//       entity: this.app.generators.createEntity(this.name),
+//     })
+//   }
+// }
